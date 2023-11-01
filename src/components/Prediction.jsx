479bcf4ba978c75b76981d0147261f5919773296
Ritiknarhe <ritiknarhe@gmail.com>
2023-11-01 15:18:24 +0530

    text

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -141,7 +141,7 @@ const Predictioin = () => {
     <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
         <div class="flex items-center pl-3">
             <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-            <label for="angular-checkbox-list" class="w-full py-3 ml-2 text-xl font-semibold text-gray-900 dark:text-gray-300">Large</label>
+            <label for="angular-checkbox-list" class="w-full py-3 ml-2 text-xl font-semibold text-gray-900 dark:text-gray-300">Largee</label>
         </div>
     </li>
     
